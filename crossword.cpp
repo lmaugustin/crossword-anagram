8e2646ca0368cb6293279e3e98a8100e6fc1e4fd
Larry Augustin <larry.augustin@gmail.com>
2016-11-19 12:23:06 -0800

    Initial load of main files that make up the solution.

---
+++ crossword.cpp
@@ -0,0 +1,23 @@
+
+/*
+ * Overall main flow:
+ *
+ * 1. Read list of words from input
+ * 2. Build anagram puzzle by placing words onto the puzzle
+ * 3. Display the solution showing all words placed
+ * 4. Display the unsolved puzzle
+ * 5. Generate the anagram clues
+ * 6. Display the anagram clues.
+ *
+ * Primary data structures:
+ *
+ * class Word_t
+ *   This is one anagram word in the puzzle.
+ * class Puzzle_t
+ *   This is a puzzle.  A puzzle is primarily a list of words.
+ *
+ */
+
+
+int main() {
+}
